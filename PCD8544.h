fe5cbb70df00be122f4ddce73c48811d9e7fc53b
Nathan Campos <eeepc904@gmail.com>
2013-07-12 02:06:52 -0300

    Reorganized the command definitions

--- PCD8544.h
+++ PCD8544.h
@@ -1,5 +1,5 @@
 /**
- *  PCD8544.c
+ *  PCD8544.
  *  PCD8544 (bit bang) driver for the MSP430G2553 based on RobG's code.
  *
  *  @author Nathan Campos <nathanpc@dreamintech.net>
@@ -8,28 +8,33 @@
 #ifndef PCD8544_H_
 #define PCD8544_H_
 
-#define PCD8544_POWERDOWN 0x04
-#define PCD8544_ENTRYMODE 0x02
-#define PCD8544_EXTENDEDINSTRUCTION 0x01
 
-#define PCD8544_DISPLAYBLANK 0x0
-#define PCD8544_DISPLAYNORMAL 0x4
-#define PCD8544_DISPLAYALLON 0x1
-#define PCD8544_DISPLAYINVERTED 0x5
+// Function Set bits (PD, V, H).
+#define PCD8544_POWERDOWN       0b00000100
+#define PCD8544_ENTRYMODE_V     0b00000010
+#define PCD8544_EXTINSTRUCTIONS 0b00000001
 
-// H = 0
-#define PCD8544_FUNCTIONSET 0x20
-#define PCD8544_DISPLAYCONTROL 0x08
-#define PCD8544_SETYADDR 0x40
-#define PCD8544_SETXADDR 0x80
+// Display Control bits (D, E).
+#define PCD8544_DISPLAYBLANK    0b00000000
+#define PCD8544_DISPLAYNORMAL   0b00000100
+#define PCD8544_DISPLAYALLON    0b00000001
+#define PCD8544_DISPLAYINVERTED 0b00000101
+
+// Command bits (H = 0).
+#define PCD8544_FUNCTIONSET    0b00100000
+#define PCD8544_DISPLAYCONTROL 0b00001000
+#define PCD8544_SETYADDR       0b01000000
+#define PCD8544_SETXADDR       0b10000000
+
+// Command bits (H = 1).
+#define PCD8544_SETTEMP 0b00000100
+#define PCD8544_SETBIAS 0b00010000
+#define PCD8544_SETVOP  0b10000000
+
+// Some other stuff?
 #define PCD8544_HPIXELS	84
 #define PCD8544_VBANKS	6
 #define PCD8544_MAXBYTES 504 // PCD8544_HPIXELS * PCD8544_VBANKS
-
-// H = 1
-#define PCD8544_SETTEMP 0x04
-#define PCD8544_SETBIAS 0x10
-#define PCD8544_SETVOP 0x80
 
 //transform
 #define NONE 0x00
@@ -39,6 +44,7 @@
 #define ROTATE_90_CW ROTATE
 #define ROTATE_90_CCW (FLIP_H | FLIP_V | ROTATE)
 #define ROTATE_180 (FLIP_H | FLIP_V)
+
 
 static const char font[][5] = {		// basic font
 	 {0x00, 0x00, 0x00, 0x00, 0x00} // 20  
